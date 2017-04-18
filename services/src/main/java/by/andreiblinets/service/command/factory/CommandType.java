8e189533a57e142070faa09230f8cabb126b74e1
Andrew <andreiblinets@gmail.com>
2017-04-18 17:33:00 +0300

    button back

--- services/src/main/java/by/andreiblinets/service/command/factory/CommandType.java
+++ services/src/main/java/by/andreiblinets/service/command/factory/CommandType.java
@@ -1,12 +1,13 @@
 package by.andreiblinets.service.command.factory;
 
 import by.andreiblinets.service.command.ICommand;
+import by.andreiblinets.service.command.user.GoBackCommand;
 import by.andreiblinets.service.command.user.GoToRegistration;
 import by.andreiblinets.service.command.user.LoginUser;
 
 public enum CommandType {
 
-    LOGIN, GOTOREGISTRATION;
+    LOGIN, GOTOREGISTRATION, BACK;
 
     public ICommand getCurrentCommand() throws EnumConstantNotPresentException{
         switch(this){
@@ -15,6 +16,9 @@ public enum CommandType {
 
             case GOTOREGISTRATION:
                 return new GoToRegistration();
+
+            case BACK:
+                return new GoBackCommand();
 
             default:
                 return new LoginUser();
--- services/src/main/java/by/andreiblinets/service/command/user/GoBackCommand.java
+++ services/src/main/java/by/andreiblinets/service/command/user/GoBackCommand.java
@@ -1,15 +1,14 @@
 package by.andreiblinets.service.command.user;
 
-
 import by.andreiblinets.service.command.ICommand;
 import by.andreiblinets.service.command.constans.PagePath;
 import by.andreiblinets.service.command.manager.ConfigurationManager;
 
 import javax.servlet.http.HttpServletRequest;
 
-public class GoToRegistration implements ICommand {
+public class GoBackCommand implements ICommand {
 
     public String execute(HttpServletRequest request) {
-        return ConfigurationManager.getInstance().getProperty(PagePath.REGISTRATION_PAGE);
+        return ConfigurationManager.getInstance().getProperty(PagePath.INDEX_PAGE);
     }
 }
